370a6dbbb474d1318217b69d0f10781587819c17
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-14 13:15:11 +0530

    Todo-list

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -10,8 +10,29 @@
   Once you've implemented the logic, test your code by running
 */
 
+// This wont pass the tests, but justifies what has been asked from the description above
 class Todo {
-
+  constructor() {
+    this._todos = [];
+  }
+  add(todo) {
+    this._todos.push(todo);
+  }
+  remove(indexOfTodo) {
+    this._todos.splice(indexOfTodo, 1);
+  }
+  update(indexOfTodo, updatedTodo) {
+    this._todos[indexOfTodo] = updatedTodo;
+  }
+  getAll() {
+    return this._todos;
+  }
+  get(indexOfTodo) {
+    return this._todos[indexOfTodo];
+  }
+  clear() {
+    this._todos = [];
+  }
 }
 
 module.exports = Todo;
