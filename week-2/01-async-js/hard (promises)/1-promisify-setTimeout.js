48f01ec2ab4436d2b474c22d1a3e94c2002533cf
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-14 19:54:03 +0530

    Resolve after n secs

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -2,7 +2,18 @@
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
 
-function wait(n) {
+function waiter(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Time is up"), n * 1000);
+    if (n < 0) {
+      reject("Given time is less than zero");
+    }
+  });
+}
+
+async function wait(n) {
+  const result = await waiter(n);
+  console.log(result);
 }
 
 module.exports = wait;
