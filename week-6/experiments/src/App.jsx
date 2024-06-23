f5350b1da4e89572d79458c3491f733cceaa6b53
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-24 00:08:17 +0530

    Render random number on click (learn re-render)

--- week-6/experiments/src/App.jsx
+++ week-6/experiments/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 
 function App() {
   const [titles, setTitles] = useState(["Title 1", "Title 2", "Title 3"])
+  const [randomName, setRandomName] = useState("Name");
   // We cant return multiple elements in react
   // So we need to return a single element
   return (
     //<> both this and <React.Fragment> are same and doesn't add any extra html tag
     <>
-      {titles.map((title, index) => <Header title={title} setTitles={setTitles} />)}
+      <button onClick={() => setRandomName(Math.random())}>Click me</button>
+      <RandomTitle title={titles[0]} randomName={randomName} />
     </>
     //</>
+  )
+}
+
+function RandomTitle({title, randomName}){
+  return (
+    <>
+    <h3>The name is {randomName}</h3>
+    <h3>The title is {title}</ h3>
+    </>
   )
 }
 
