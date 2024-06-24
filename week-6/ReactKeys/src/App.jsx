4eb7883e990d4e4ece65a57c4a9af2e2bf9fb2e1
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-24 16:56:12 +0530

    Add docstring for quick look

--- week-6/ReactKeys/src/App.jsx
+++ week-6/ReactKeys/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 
+/*
+Warning: Each child in a list should have a unique "key" prop.
+
+Check the render method of `App`. See https://reactjs.org/link/warning-keys for more information.
+*/
+
 function App() {
   const [todos, setTodos] = useState([
     {title: "Learn React", description: "Forget react", id: 1}, 
     {title: "Learn Vue", description: "Forget vue", id: 2}, 
     {title: "Learn Angular", description: "Forget angular", id: 3},
   ])
-
+  /* 
+  If we dont add a key to a component, especially when using a array, number of re-renders may increase and it is not efficient
+  Keys tell React which array item each component corresponds to, so that it can match them up later. 
+  This becomes important if your array items can move (e.g. due to sorting), get inserted, or get deleted. 
+  A well-chosen key helps React infer what exactly has happened, and make the correct updates to the DOM tree.
+  */
   return (
     <>
     <button onClick={function createNewTodo(){
@@ -17,14 +28,14 @@ function App() {
       ]
     )}}>Add Todo
     </button>
-    {todos.map((todo) => <RenderTodo title={todo.title} description={todo.description} id={todo.id}/>)}
+    {todos.map((todo) => <RenderTodo key={todo.id} title={todo.title} description={todo.description}/>)}
     </>
   )
 }
 
-function RenderTodo({title, description, id}){
+function RenderTodo({title, description}){
   return (
-    <div key={id}>
+    <div>
      <h1>{title}</h1>
      <p>{description}</p>
     </div>
