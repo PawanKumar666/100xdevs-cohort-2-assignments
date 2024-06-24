830947380896fdbbf78999ce7e657de8ef10a693
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-24 16:08:57 +0530

    React.memo def

--- week-6/experiments/src/App.jsx
+++ week-6/experiments/src/App.jsx
@@ -1,30 +1,43 @@
 import { useState } from 'react'
+import React from 'react';
 
 function App() {
   const [titles, setTitles] = useState(["Title 1", "Title 2", "Title 3"])
-  const [randomName, setRandomName] = useState("Name");
   // We cant return multiple elements in react
   // So we need to return a single element
+  // If any state of the parent changes it re-renders
+  // Anytime the parent re-renders - all its children re-render (if they are not memoized)
+  // Define the state in lowest ancestor from which it is needed
   return (
     //<> both this and <React.Fragment> are same and doesn't add any extra html tag
     <>
-      <button onClick={() => setRandomName(Math.random())}>Click me</button>
-      <RandomTitle title={randomName} />
-      <RandomTitle title={titles[0]} />
+      <RandomTitle />
+      {titles.map((title) => <MemoHeader title={title} />)}
     </>
     //</>
   )
 }
 
-function RandomTitle({title}){
+function RandomTitle(){
+  const [randomName, setRandomName] = useState("Name");
   return (
     <>
-    <h3>The title is {title}</ h3>
+    <button onClick={() => setRandomName(Math.random())}>Click me</button>
+    <h3>The name is {randomName}</h3>
     </>
   )
 }
 
-function Header({title, setTitles}){
+//memoization which prevents re-rendering of the component if the props are the same
+const MemoHeader = React.memo(Header);
+
+function Header({title}){
+  return (
+    <h3>The title is {title}</h3>
+  )
+}
+
+function HeaderButton({title, setTitles}){
   return (
     <button style={{color: "red", fontSize: "20px", padding: "10px", margin: "10px"}} onClick={() => setTitles(prevTitles => [...prevTitles, "New Title " + (prevTitles.length + 1)])}>{title}</button>
   )
