20e87e54b31397c59d1c93ca35cde37514652418
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-25 19:00:06 +0530

    Render random todos polling every 5 secs

---
+++ week-6/ReactKeys/src/AppRandomTodoRenderer.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from "react";
+
+function TodoRenderer({title, description}){
+    return (
+        <>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        </>
+    )
+}
+
+function TodoCardWrapper({children}){
+    return <div style={{border:"2px solid black", margin:"10px", padding:"10px"}}>{children}</div>
+}
+
+function App(){
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            fetch("https://sum-server.100xdevs.com/todos")
+            .then(async (res) => {
+            const value = await res.json();
+            setTodos(value.todos);
+        })
+        }, 5000);
+        return () => clearInterval(intervalId);
+    }, [])
+
+    return (
+        <div>
+            {todos && todos.map((todo) => {
+                        return (<TodoCardWrapper key={todo.id}>
+                            <TodoRenderer title={todo.title} description={todo.description} />
+                        </TodoCardWrapper>)
+                })}
+        </div>
+    )
+}
+
+export default App;
--- week-6/ReactKeys/src/main.jsx
+++ week-6/ReactKeys/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AppWrapperComp from './AppWrapperComp.jsx'
+import AppRandomTodoRenderer from './AppRandomTodoRenderer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppWrapperComp />
+    <AppRandomTodoRenderer />
   </React.StrictMode>,
 )
