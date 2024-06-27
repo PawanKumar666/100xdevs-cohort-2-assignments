136856c1877036c41c71178b4052766a194cd260
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-27 20:59:05 +0530

    Debounced sum of n

--- week-6/ReactKeys/src/AppSumOfN.jsx
+++ week-6/ReactKeys/src/AppSumOfN.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App(){
+    const [n, setN] = useState(0);
     const [sum, setSum] = useState(0);
     const [counterNum, setCounterNum] = useState(0);
-    function OnInputChange(e){
-        console.log(e)
-        const sumOfN = (e * (e + 1))/2;
+    const [debouncedN, setDebouncedN] = useState(null);
+
+    useEffect(() => {
+        const handler = setTimeout (() => {
+            setDebouncedN(n);
+        }, 500)
+
+        return () => clearTimeout(handler);
+    }, [n]);
+
+    useEffect(() => {
+        const sumOfN = (debouncedN * (debouncedN + 1))/2;
         setSum(sumOfN);
-    }
+    }, [debouncedN]);
+
     return (
         <>
-        <input type="number" onChange={(e) => OnInputChange(parseInt(e.target.value))} />
+        <input type="number" onChange={(e) => setN(parseInt(e.target.value))} />
         <p>Sum: {sum}</p>
         <button onClick={() => setCounterNum(counterNum + 1)}>Counter {counterNum}</button>
         </>
