43fc48f4139d55b4db0390a217f8937afe2cae1e
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-27 20:28:56 +0530

    Reduce load by not loading entire packages

--- week-6/ReactKeys/src/AppGetIdFromButton.jsx
+++ week-6/ReactKeys/src/AppGetIdFromButton.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import {useState, useEffect} from "react";
+import get from "axios";
 
 function App(){
     return <GatherIdAndPopulateTodo />
@@ -10,7 +10,7 @@ function GatherIdAndPopulateTodo(){
     const [todo, setTodo] = useState(null);
 
     useEffect(() => {
-        axios.get(`https://sum-server.100xdevs.com/todo?id=${todoId}`)
+        get(`https://sum-server.100xdevs.com/todo?id=${todoId}`)
         .then((res) => {
             setTodo(res.data.todo);
         })
