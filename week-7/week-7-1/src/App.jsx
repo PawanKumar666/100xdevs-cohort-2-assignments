fb38e51c74193b6235b8a08d923596a04f8a1250
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-29 19:52:21 +0530

    Lazy loading example

--- week-7/week-7-1/src/App.jsx
+++ week-7/week-7-1/src/App.jsx
@@ -1,7 +1,15 @@
+import { lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from '../components/LandingPage';
-import Dashboard from '../components/Dashboard';
-import NavBar from '../components/NavBar';
+import LandingPage from './components/LandingPage';
+import NavBar from './components/NavBar';
+// Why to load all the components and create a big js file to send the user at once, instead of loading the components on demand?
+// Answer: It is not a good idea to load all the components and create a big js file to send the user at once, instead of loading the components on demand.
+// It is better to load the components on demand and send the user to the desired page.
+// Lazy loading in react solves this
+
+// import Dashboard from React.lazy(() => import('../components/Dashboard'));
+
+const Dashboard = lazy(() => import("./components/Dashboard"));
 
 function App() {
 
