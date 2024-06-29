95e56abaa0b226cb1e05433d07b17e9a8b875bed
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-06-29 17:20:44 +0530

    useCallback assignment 1

--- week-6/2-use-callback/src/components/Assignment1.jsx
+++ week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
@@ -6,13 +6,19 @@ export function Assignment1() {
     const [count, setCount] = useState(0);
 
     // Your code starts here
-    function handleIncrement() {
+    const handleIncrement = useCallback(() => {
+        // setCount(count + 1);
+        setCount(function(currentCount){
+            return currentCount + 1;
+        })
+    }, []); // The benefit of useCallback is utilised in this particular problem only if we dont have count in the dependency array.
+    // Instead of using setCount(count+1) which accesses the current state, we use setCount(function(currentCount){return currentCount + 1;})
+    // which ensures that the latest state is captured by the callback. and this helps in ignoring count from the dependancy array
 
-    }
-
-    function handleDecrement() {
-        
-    }
+    const handleDecrement = useCallback(() => {
+        // setCount(count - 1);
+        setCount((currentCount) => currentCount - 1);
+    }, []);
     // Your code ends here
 
     return (
