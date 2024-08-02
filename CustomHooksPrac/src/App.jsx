6f2659b5689207c6e071790116e453d43d51450e
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-08-03 00:03:09 +0530

    CustomHook prac

---
+++ CustomHooksPrac/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+function App() {
+  return (
+    <>
+    <CountComponent />
+    <CountComponentClass />
+    </>
+  )
+}
+
+function CountComponent(){
+  const [count, setCount] = useState(0)
+  return (
+    <>
+    <h1>Function Component</h1>
+    <h1>Count: {count}</h1>
+    <button className='bg-blue-500 text-white p-2 rounded-md' onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  )
+}
+
+class CountComponentClass extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {count: 0}
+  }
+  render(){
+    return (
+      <>
+      <h1>Class Component</h1>
+      <h1>Count: {this.state.count}</h1>
+      <button className='bg-blue-500 text-white p-2 rounded-md' onClick={() => this.setState({count: this.state.count + 1})}>Increment</button>
+      </>
+    )
+  }
+}
+
+export default App;
