aa13f58e17b6e328ec6a1f08b9a98fc9db364c9e
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-08-05 23:24:09 +0530

    Note

--- Typescript-Intro/src/App.tsx
+++ Typescript-Intro/src/App.tsx
@@ -12,4 +12,11 @@ function greetWithFirstName(firstName: string): string {
   return `Hello ${firstName}`;
 }
 
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+const res = sum(1, 2); // Type for res is automatically inferred as number and 
+// if we hover over invocation of sum method we can see the return type is automatically inferred as number
+
 export default App;
