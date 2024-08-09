31cdc9e1c61ae090d16e86bed0338828391c7963
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-08-09 16:29:16 +0530

    Doc

--- week-10/1-postgres-simple/src/index.ts
+++ week-10/1-postgres-simple/src/index.ts
@@ -1,4 +1,4 @@
-import { Client } from 'pg'
+import { Client } from 'pg' // Widely used library for postgres usage in express
 import { DB_URL } from './config';
 
 export const client = new Client({
