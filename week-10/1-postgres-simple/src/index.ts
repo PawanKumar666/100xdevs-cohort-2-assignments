39fe6abc09435aafe78ca47db0b7c7dc109e1515
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-08-11 22:00:40 +0530

    Tag todo

--- week-10/1-postgres-simple/src/index.ts
+++ week-10/1-postgres-simple/src/index.ts
@@ -2,6 +2,13 @@ import { Client } from 'pg' // Widely used library for postgres usage in express
 import { DB_URL } from './config';
 import { createTables } from './db/setup';
 
+/*
+TODO:
+- Create a new table called "Clients" with the following columns:
+    - id: SERIAL PRIMARY KEY
+    - Account name: VARCHAR(255) NOT NULL
+    - password: VARCHAR(255) NOT NULL
+*/
 console.log(DB_URL);
 export const client = new Client({
     connectionString: DB_URL
